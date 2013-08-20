a6b84c979ff4183d3a7809e28c79e0ceeabf5343
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-08-20 22:14:07 +0100

    Draw small area at max detail

--- terrain.py
+++ terrain.py
@@ -51,6 +51,58 @@ class QuadTileData:
 		self.latRange = self.latLims[1] - self.latLims[0]
 		self.lonRange = self.lonLims[1] - self.lonLims[0]
 
+class GeoMipMap():
+	def __init__(self, data, xinds = None, yinds = None, depth = 0):
+		
+		self.data = data
+		self.depth = depth
+		if xinds is not None: self.xinds = xinds
+		else: self.xinds = (0, data.tile.shape[0]-1)
+		if yinds is not None: self.yinds = yinds
+		else: self.yinds = (0, data.tile.shape[1]-1)
+
+	def Draw(self):
+		for i in range(self.xinds[0], self.xinds[1]):
+			for j in range(self.yinds[0], self.yinds[1]):
+
+				x1 = -float(i) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[1]
+				x2 = -float(i+1) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[1]
+				y1 = float(j) * self.data.lonRange / self.data.tile.shape[1] + self.data.lonLims[0]
+				y2 = float(j+1) * self.data.lonRange / self.data.tile.shape[1] + self.data.lonLims[0]
+
+				p11 = self.data.proj.Proj(x1, y1, 0.)
+				p12 = self.data.proj.Proj(x1, y2, 0.)
+				p21 = self.data.proj.Proj(x2, y1, 0.)
+				p22 = self.data.proj.Proj(x2, y2, 0.)
+
+				c11 = self.data.tile[i, j] 
+				c12 = self.data.tile[i, j]
+				c21 = self.data.tile[i, j]
+				c22 = self.data.tile[i, j]
+
+
+				if 1:
+					glBegin(GL_QUADS)
+					glColor4f(c11, c11, c11, 1.)
+					glVertex(*p11)
+					glColor4f(c21, c21, c21, 1.)
+					glVertex(*p21)
+					glColor4f(c22, c22, c22, 1.)
+					glVertex(*p22)
+					glColor4f(c12, c12, c12, 1.)
+					glVertex(*p12)
+					glEnd()
+
+				if 0:
+					glColor4f(1., 1., 0., 1.)
+					glBegin(GL_LINE_LOOP)
+					glVertex(*p11)
+					glVertex(*p21)
+					glVertex(*p22)
+					glVertex(*p12)
+					glEnd()
+
+
 class QuadTile():
 	def __init__(self, data, xinds = None, yinds = None, depth = 0):
 
@@ -65,10 +117,11 @@ class QuadTile():
 		if self.xinds[1]-self.xinds[0] > 50 or self.yinds[1]-self.yinds[0] > 50:
 			self.Split()
 
+
 	def Draw(self):
 
-		x1 = float(self.xinds[0]) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[0]
-		x2 = float(self.xinds[1]) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[0]
+		x1 = -float(self.xinds[0]) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[1]
+		x2 = -float(self.xinds[1]) * self.data.latRange / self.data.tile.shape[0] + self.data.latLims[1]
 		y1 = float(self.yinds[0]) * self.data.lonRange / self.data.tile.shape[1] + self.data.lonLims[0]
 		y2 = float(self.yinds[1]) * self.data.lonRange / self.data.tile.shape[1] + self.data.lonLims[0]
 
@@ -94,7 +147,7 @@ class QuadTile():
 				glColor4f(c12, c12, c12, 1.)
 				glVertex(*p12)
 				glEnd()
-			if 1:
+			if 0:
 				glColor4f(1., 1., 0., 1.)
 				glBegin(GL_LINE_LOOP)
 				glVertex(*p11)
@@ -115,6 +168,16 @@ class QuadTile():
 		self.childTiles.append(QuadTile(self.data, (xsp, self.xinds[1]), (self.yinds[0], ysp), self.depth + 1))
 		self.childTiles.append(QuadTile(self.data, (self.xinds[0], xsp), (ysp, self.yinds[1]), self.depth + 1))
 		self.childTiles.append(QuadTile(self.data, (xsp, self.xinds[1]), (ysp, self.yinds[1]), self.depth + 1))
+
+	def SplitToMipMap(self):
+		xsp = int(round((self.xinds[0] + self.xinds[1]) * 0.5))
+		ysp = int(round((self.yinds[0] + self.yinds[1]) * 0.5))
+
+		self.childTiles = []
+		self.childTiles.append(GeoMipMap(self.data, (self.xinds[0], xsp), (self.yinds[0], ysp), self.depth + 1))
+		self.childTiles.append(GeoMipMap(self.data, (xsp, self.xinds[1]), (self.yinds[0], ysp), self.depth + 1))
+		self.childTiles.append(GeoMipMap(self.data, (self.xinds[0], xsp), (ysp, self.yinds[1]), self.depth + 1))
+		self.childTiles.append(GeoMipMap(self.data, (xsp, self.xinds[1]), (ysp, self.yinds[1]), self.depth + 1))
 
 def run():
 	
@@ -141,6 +204,12 @@ def run():
 	tileData = QuadTileData(tileNorm, 51., 52., 1., 2.)
 	srtmTile = QuadTile(tileData)
 
+	count = 0
+	ti = srtmTile
+	while len(ti.childTiles) > 0:
+		ti = ti.childTiles[0]
+	ti.SplitToMipMap()
+
 	while True:
 		
 		time_passed = clock.tick()
@@ -155,13 +224,13 @@ def run():
 		pressed = pygame.key.get_pressed()
 
 		if pressed[K_LEFT]:
-			camLatLon[1] -= 1. * time_passed_seconds
+			camLatLon[1] -= 0.1 * time_passed_seconds
 		if pressed[K_RIGHT]:
-			camLatLon[1] += 1. * time_passed_seconds
+			camLatLon[1] += 0.1 * time_passed_seconds
 		if pressed[K_UP]:
-			camLatLon[0] += 1. * time_passed_seconds
+			camLatLon[0] += 0.1 * time_passed_seconds
 		if pressed[K_DOWN]:
-			camLatLon[0] -= 1. * time_passed_seconds
+			camLatLon[0] -= 0.1 * time_passed_seconds
 		if pressed[K_a]:
 			camAlt -= 100000. * time_passed_seconds
 		if pressed[K_z]:
