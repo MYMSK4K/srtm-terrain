9b16885b74e70bae44295f0e0330e4913d8e2815
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-08-26 20:17:16 +0100

    Implement click drag

--- gui.py
+++ gui.py
@@ -10,16 +10,19 @@ class Gui(events.EventCallback):
 		mediator.AddListener("mousedowntimeout", self)
 		self.playerId = None
 		self.faction = None
-		self.mousePressEvents = {}
+		self.mouseDownEvents = {}
+		self.mouseDownStart = {}
 		#self.multiClickDriftTol = 5. #px
 		self.multiClickTimeout = .2 #sec
 
 	def ProcessEvent(self, ev):
 		#print event.type
 		if ev.type == "mousebuttondown":
-			if ev.button not in self.mousePressEvents:
-				self.mousePressEvents[ev.button] = []
-			self.mousePressEvents[ev.button].append(ev)
+			if ev.button not in self.mouseDownEvents:
+				self.mouseDownEvents[ev.button] = []
+			self.mouseDownEvents[ev.button].append(ev)
+
+			self.mouseDownStart[ev.button] = ev
 			
 			timeOutEvent = events.Event("mousedowntimeout")
 			timeOutEvent.button = ev.button
@@ -30,23 +33,33 @@ class Gui(events.EventCallback):
 		if ev.type == "mousedowntimeout":
 			
 			#Check if later clicks will be performing the time out
-			prevClick = self.mousePressEvents[ev.button][-1]
+			prevClick = self.mouseDownEvents[ev.button][-1]
 			if ev.triggerTime != prevClick.time:
 				return
 
 			#Dispatch clicks
-			if ev.button == 1 and len(self.mousePressEvents[ev.button])==1:
+			if ev.button == 1 and len(self.mouseDownEvents[ev.button])==1 and self.mouseDownStart[ev.button] is None:
 				self.SingleLeftClick(prevClick.screenPos, prevClick.worldPos)
 
-			if ev.button == 3 and len(self.mousePressEvents[ev.button])==1:
+			if ev.button == 3 and len(self.mouseDownEvents[ev.button])==1 and self.mouseDownStart[ev.button] is None:
 				self.SingleRightClick(prevClick.screenPos, prevClick.worldPos)
 
 			#Clear click time log
-			self.mousePressEvents[ev.button] = []
+			self.mouseDownEvents[ev.button] = []
 
 		if ev.type == "mousebuttonup":
 			print ev.type
-	
+			self.mouseDownStart[ev.button] = None
+
+		if ev.type == "mousemotion":
+			pressed = False
+			for button in self.mouseDownStart:
+				if self.mouseDownStart[button] is not None: pressed = True
+
+			if not pressed: return
+			#Drag event
+
+			
 	def SingleLeftClick(self, screenPos, worldPos):
 		moveOrder = events.Event("moveorder")
 		moveOrder.pos = (worldPos[0], worldPos[1], 0.)
